b0bb00fd3f1647646773452be866e5e38f299fb8
Prashanth Mundkur <prashanth@riscv.org>
2026-03-20 08:28:32 -0500

    This fixes several issues:
    - There was no easy way to test the workflow locally. This is fixed by
      putting most of the build logic in a justfile and using it from the
      workflows. The justfile (and an accompanying run.sh script) allows
      local packaging and testing of the test archives.
    
    - There was too much duplication between the CI (compile.yml) and
      Release (confusingly called build-riscv-tests.yml) workflows. This
      is fixed by making the new CI workflow (build.yml) be reused by the
      Release (release.yml) workflow. The Release workflow now contains no
      build logic at all.
    
    - The caches did not work at all in the previous workflows, making
      them pointless and increasing the duration of every build. The
      caches in the new workflows have been tested to work.
    
    - The setting of MARCH variable for the vector-tests has been removed,
      since it now exactly matches upstream. This means that if upstream
      adds more tests and extends their MARCH, these tests would now be
      built automatically here too (instead of being silently
      excluded). This might result in failing tests for the sail-riscv
      emulator, giving a good heads up to support the new tests. This may
      also cause increased test archive size causing issues with artifact
      {up,down}loads, so we might have to reconsider this if that happens.
    
    The new workflows are in their own files. The old workflows can be
    deleted in a subsequent PR.
    
    This also fixes #31.

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+set -euxo pipefail
+
+# This script just runs the whole build workflow locally.
+
+INSTALL_PREFIX=`realpath ../sail-riscv-tests-install`
+# The `just` package on Ubuntu 24.04 doesn't support the script attribute.
+JUST="just --unstable"
+
+# Install the toolchain.
+${JUST} install-toolchain ${INSTALL_PREFIX}
+
+# Make it available.
+# The `just` package on Ubuntu 24.04 doesn't support the env attribute.
+export PATH=${INSTALL_PREFIX}/riscv/bin:${PATH}
+
+# riscv-tests
+${JUST} riscv-tests-tgz ${INSTALL_PREFIX}
+#${JUST} cleanup-riscv-tests ${INSTALL_PREFIX}
+
+# Set up Spike install.
+# The `just` package on Ubuntu 24.04 doesn't support the env attribute.
+export RISCV=${INSTALL_PREFIX}/riscv
+
+# Spike
+${JUST} build-spike ${INSTALL_PREFIX}
+#${JUST} clean-spike
+
+# riscv-vector-tests
+${JUST} vector-tests-tgz 128 32
+${JUST} vector-tests-tgz 128 64
+${JUST} vector-tests-tgz 256 32
+${JUST} vector-tests-tgz 256 64
+${JUST} vector-tests-tgz 512 32
+${JUST} vector-tests-tgz 512 64
+#${JUST} clean-vector-tests
